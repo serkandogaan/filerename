83ae65f0148f29f99ecd94325e131de049aacbc6
serkandogaan <serkan.dogan333@outlook.com>
2023-07-15 09:38:50 +0300

    cli added v2

--- filerename.go
+++ filerename.go
@@ -59,38 +59,4 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// rename boşluk dosya yolu vereceksin cli kobra ile yap.
-
-	// filePath := "C:/Users/Serkan/Desktop/dosyalar" // Dosya yaolunu buraya girin
-	// // var input string
-	// // fmt.Scanln(&input)
-	// // filePath = input
-
-	// files, err := ioutil.ReadDir(filePath)
-	// if err != nil {
-	// 	fmt.Println("Klasör okunamadi, Dosya yolunu tekrar girin : ", err)
-	// 	return
-	// }
-
-	// for i, file := range files {
-
-	// 	oldName := file.Name()
-	// 	newName := strconv.Itoa(i + 1)
-
-	// 	if i+1 < 10 {
-	// 		newName = "0" + newName
-	// 	}
-	// 	newName = newName + filepath.Ext(oldName)
-
-	// 	oldPath := filepath.Join(filePath, oldName) // isim değişmeden önceki yol adresi
-	// 	newPath := filepath.Join(filePath, newName) // isim değişince oluşacak yol adresi
-
-	// 	err = os.Rename(oldPath, newPath)
-	// 	if err != nil {
-	// 		fmt.Printf("Değiştirilemedi %s", err)
-	// 	} else {
-	// 		fmt.Printf("Değiştirildi %s \n", newName)
-	// 	}
-	// }
-
 }
